0781ad8f18681a964f142441aa37820e03ae033e
walter30ciancio <walterciancio@hotmail.com>
2026-06-27 12:32:30 -0300

    Ultimas modificaciones antes de subir

--- controllers/pedidos.controller.js
+++ controllers/pedidos.controller.js
@@ -1,6 +1,7 @@
 const Pedido = require('../models/Pedido');
+const Producto = require('../models/Producto');
 
-// 1. OBTENER TODOS LOS PEDIDOS
+// OBTENER TODOS LOS PEDIDOS
 const getPedidos = async (req, res) => {
     try {
         const pedidos = await Pedido.find().populate('productos');
@@ -11,25 +12,57 @@ const getPedidos = async (req, res) => {
     }
 };
 
-// 2. CREAR UN NUEVO PEDIDO
+// CREAR UN NUEVO PEDIDO
 const createPedido = async (req, res) => {
     try {
-        const { cliente, productos, total, estado } = req.body;
+        // Ya no extraemos el 'total' del req.body
+        const { cliente, productos, estado } = req.body;
+
+        if (!productos || productos.length === 0) {
+            return res.status(400).send("Un pedido debe contener al menos un producto.");
+        }
+
+        // Buscamos en la base de datos los productos reales usando los IDs
+        const productosDb = await Producto.find({ _id: { $in: productos } });
+
+        // Calculamos el total automáticamente
+        let totalCalculado = 0;
+        console.log("--- INICIANDO CÁLCULO DE PEDIDO ---");
+
+        productos.forEach(id => {
+            const idLimpio = id.trim();
+            const productoEncontrado = productosDb.find(p => p._id.toString() === idLimpio);
+            
+            if (productoEncontrado) {
+                const precioReal = Number(productoEncontrado.precio);
+                totalCalculado += precioReal;
+                console.log(`✅ Sumando: ${productoEncontrado.nombre} -> $${precioReal}`);
+            } else {
+                console.log(`❌ ALERTA: No se encontró el ID: ${idLimpio}`);
+            }
+        });
+
+        console.log("2. TOTAL FINAL CALCULADO: $", totalCalculado);
+        console.log("-----------------------------------");
+
+        // 4. Creamos el pedido inyectando el total calculado por nosotros
         const nuevoPedido = new Pedido({
             cliente,
             productos,
-            total,
+            total: totalCalculado,
             estado: estado || 'pendiente'
         });
+
         await nuevoPedido.save();
         res.redirect('/pedidos');
+
     } catch (error) {
         console.error("Error al crear pedido:", error);
         res.status(400).send("Error al guardar el pedido. Verifique las validaciones.");
     }
 };
 
-// 3. OBTENER UN PEDIDO POR SU ID
+// OBTENER UN PEDIDO POR SU ID
 const getPedidoById = async (req, res) => {
     try {
         const pedido = await Pedido.findById(req.params.id).populate('productos');
